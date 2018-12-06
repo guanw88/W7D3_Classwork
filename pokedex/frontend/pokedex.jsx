8131f9bc70d101cd9393bcfdd33d8839839e848c
guanw88 <guanw88@gmail.com>
2018-12-05 16:30:44 -0800

    Add selectAllPokemon selector and complete redux cycle

--- pokedex/frontend/pokedex.jsx
+++ pokedex/frontend/pokedex.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { fetchAllPokemon } from './util/api_util';
 import { receiveAllPokemon, RECEIVE_ALL_POKEMON, requestAllPokemon } from './actions/pokemon_actions';
 import configureStore from "./store/store";
+import { selectAllPokemon } from './reducers/selectors';
 
 document.addEventListener('DOMContentLoaded', () => {
   // testing
@@ -12,6 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
   window.getState = store.getState;
   window.dispatch = store.dispatch;
   window.requestAllPokemon = requestAllPokemon;
+  window.selectAllPokemon = selectAllPokemon;
 
 
   const root = document.getElementById('root');
