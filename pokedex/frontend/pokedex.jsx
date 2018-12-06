453d6f18d1db19cc1c61d1d2e9b0db8215bcdc64
guanw88 <guanw88@gmail.com>
2018-12-05 16:53:34 -0800

    Add root component to entry file

--- pokedex/frontend/pokedex.jsx
+++ pokedex/frontend/pokedex.jsx
@@ -4,18 +4,19 @@ import { fetchAllPokemon } from './util/api_util';
 import { receiveAllPokemon, RECEIVE_ALL_POKEMON, requestAllPokemon } from './actions/pokemon_actions';
 import configureStore from "./store/store";
 import { selectAllPokemon } from './reducers/selectors';
+import Root from "./components/root";
 
 document.addEventListener('DOMContentLoaded', () => {
   // testing
-  window.receiveAllPokemon = receiveAllPokemon;
-  window.fetchAllPokemon = fetchAllPokemon;
+  // window.receiveAllPokemon = receiveAllPokemon;
+  // window.fetchAllPokemon = fetchAllPokemon;
+  // window.getState = store.getState;
+  // window.dispatch = store.dispatch;
+  // window.requestAllPokemon = requestAllPokemon;
+  // window.selectAllPokemon = selectAllPokemon;
+
+
   const store = configureStore();
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
-  window.requestAllPokemon = requestAllPokemon;
-  window.selectAllPokemon = selectAllPokemon;
-
-
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>HELLO</h1>, root);
+  ReactDOM.render(<Root store={store} />, root);
 });
