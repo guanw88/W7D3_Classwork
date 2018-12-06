299e9f6e107ba1fdedbca36c2ee49bba142dbef8
guanw88 <guanw88@gmail.com>
2018-12-05 16:13:19 -0800

    Add actions, reducers, and store for fetchAllPokemon

--- pokedex/frontend/pokedex.jsx
+++ pokedex/frontend/pokedex.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { fetchAllPokemon } from './util/api_util';
+import { receiveAllPokemon, RECEIVE_ALL_POKEMON } from './actions/pokemon_actions';
+import configureStore from "./store/store";
 
 document.addEventListener('DOMContentLoaded', () => {
+  // testing
+  window.receiveAllPokemon = receiveAllPokemon;
+  window.fetchAllPokemon = fetchAllPokemon;
+  const store = configureStore();
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+
+
   const root = document.getElementById('root');
   ReactDOM.render(<h1>HELLO</h1>, root);
 });
